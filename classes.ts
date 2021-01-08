8e2fadc5840d7f3b293ee4c5d179ffea31821356
sdawes437 <sdawes437@gmail.com>
2021-01-08 17:51:10 +0000

    First commit

---
+++ classes.ts
@@ -0,0 +1,25 @@
+class Vehicle {
+  constructor(public color: string) {}
+
+  protected honk(): void {
+    console.log('beep');
+  }
+}
+
+const vehicle = new Vehicle('orange');
+console.log(vehicle.color);
+
+/* class Car extends Vehicle {
+  private drive(): void {
+    console.log('vroom');
+  }
+
+  startDrivingProcess(): void {
+    this.drive();
+    this.honk();
+  }
+}
+
+const car = new Car();
+car.startDrivingProcess();
+*/
